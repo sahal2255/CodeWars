cf723cb1e655ff1c0a069c99f9917470df290447
sahal2255 <sahalvv9656@gmail.com>
2024-02-07 09:30:30 +0530

    edited two

--- Two.js
+++ Two.js
@@ -2,11 +2,14 @@ function peopleDrink(age){
     if(age<14){
         return 'drink toddy'
     }
-    if(age<17){
+    if(age<=17){
         return 'drink coke'
     }
-    if(age<18){
+    if(age<=20){
         return 'drink beer'
+    }
+    if(age=>20){
+        return 'drink whisky'
     }
     else{
         return 'drink whisky'
