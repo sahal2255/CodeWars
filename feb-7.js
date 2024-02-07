92be557c429b1cae39b09dbacfcb03bc1b5270f5
sahal2255 <sahalvv9656@gmail.com>
2024-02-07 09:46:35 +0530

    feb-7

---
+++ feb-7.js
@@ -0,0 +1,25 @@
+// function peopleDrink(age){
+//     if(age<14){
+//         return 'drink toddy'
+//     }
+//     if(age<=17){
+//         return 'drink coke'
+//     }
+//     if(age<=20){
+//         return 'drink beer'
+//     }
+//     if(age=>20){
+//         return 'drink whisky'
+//     }
+//     else{
+//         return 'drink whisky'
+//     }
+// }
+// console.log(peopleDrink(13))
+
+
+
+function litre(time){
+    return Math.floor(time*0.5)
+}
+console.log(litre(3));
