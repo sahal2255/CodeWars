2e9a7cf006a87da014675aabd0d51a31698e810a
sahal2255 <sahalvv9656@gmail.com>
2024-02-09 09:33:17 +0530

    feb-9

---
+++ feb-9.js
@@ -0,0 +1,17 @@
+// function doubleInteger(i){
+//     return i*2
+// }
+// console.log(doubleInteger(2));
+
+
+function problem(x){
+    if(typeof x==='number'){
+        const result = x*50;
+        const res= result+6
+        return res
+    }
+    else{
+        return 'Error'
+    }
+}
+console.log(problem(5));
