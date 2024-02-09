1a1b1d191ea42ce5eba95541700c45a5a2ce9b1c
sahal2255 <sahalvv9656@gmail.com>
2024-02-09 10:01:14 +0530

    second

--- feb-9.js
+++ feb-9.js
@@ -4,14 +4,31 @@
 // console.log(doubleInteger(2));
 
 
-function problem(x){
-    if(typeof x==='number'){
-        const result = x*50;
-        const res= result+6
-        return res
+// function problem(x){
+//     if(typeof x==='number'){
+//         const result = x*50;
+//         const res= result+6
+//         return res
+//     }
+//     else{
+//         return 'Error'
+//     }
+// }
+// console.log(problem(5));
+
+
+
+function saleHotdogs(n){
+    var condition=10
+    var statement1=20
+      if(n===condition){
+        return "statementa"
+      }
+      if( n <100){
+        return "statementb"
+      }
+      else {
+        return "st"
+      }
     }
-    else{
-        return 'Error'
-    }
-}
-console.log(problem(5));
+    console.log(saleHotdogs(90))
