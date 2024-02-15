6bd394095f952ec90e39bc1f31cec3c90f80a8ee
sahal2255 <sahalvv9656@gmail.com>
2024-02-15 09:45:50 +0530

    feb-15

--- feb-14.js
+++ feb-14.js
@@ -13,12 +13,12 @@
 
 
 
-function simpleMultiplication(number) {
-    if (number %2===0){
-      return number*8
-    }
-    else{
-      return number*9
-    }
-}
-console.log(simpleMultiplication(3));
+// function simpleMultiplication(number) {
+//     if (number %2===0){
+//       return number*8
+//     }
+//     else{
+//       return number*9
+//     }
+// }
+// console.log(simpleMultiplication(3));
---
+++ feb-15.js
@@ -0,0 +1,20 @@
+// function expressionMatter(a, b, c) {
+    
+//     const result1 = a * (b + c);
+//     const result2 = a * b * c;
+//     const result3 = a + b * c;
+//     const result4 = (a + b) * c;
+//     const result5 = a + b + c;
+
+//     return Math.max(result1, result2, result3, result4, result5);
+// }
+
+// console.log(expressionMatter(2, 3, 4))
+
+
+
+function addFive(num) {
+    var total = num + 5
+    return total
+  }
+  console.log(addFive(5))
