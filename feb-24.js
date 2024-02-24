9f7185d6743a9170a9b189814ed04bbfc314c100
sahal2255 <sahalvv9656@gmail.com>
2024-02-24 09:29:53 +0530

    today

---
+++ feb-24.js
@@ -0,0 +1,13 @@
+function multiTable(number) {
+    let table = '';
+    for (let i = 1; i <= 10; i++) {
+      table += `${i} * ${number} = ${i * number}`;
+      if (i !== 10) {
+        table += '\n\n'; 
+      }
+    }
+    return table;
+  }
+  
+  console.log(multiTable(5));
+  
