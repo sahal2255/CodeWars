dce59d94d6a72f154a87617579d129940a859e32
sahal2255 <sahalvv9656@gmail.com>
2024-03-06 09:42:11 +0530

    mar-6

---
+++ mar-6.js
@@ -0,0 +1,18 @@
+// function reverseLetter(str) {
+//     const main =str.replace(/[^a-zA-Z]/g,'')
+//     const out=main.split('').reverse('').join('')
+//     return out    
+//   }
+//   console.log(reverseLetter("krishan"));
+//   console.log(reverseLetter("ltr53o?n"));
+
+
+
+
+
+
+
+// function distinct(a) {
+
+//     }
+//     console.log(distinct([1, 1, 2]));
