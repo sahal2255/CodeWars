1821dce641a93bcb26375231a2a9470be12319fa
sahal2255 <sahalvv9656@gmail.com>
2024-03-07 10:06:19 +0530

    mar7-

--- mar-6.js
+++ mar-6.js
@@ -13,7 +13,11 @@
 
 
 // function distinct(a) {
-    return [...new Set(a)]
+    // return [...new Set(a)]
 //     }
 //     console.log(distinct([1, 1, 2]));
+
+
+
+
 
---
+++ mar-7.js
@@ -0,0 +1,15 @@
+// function disemvowel(str) {
+//     return str.replace(/[aeiouAEIOU]/g,'');
+//   }
+// //   console.log(disemvowel("This website is for losers LOL!"))
+// const result=disemvowel("This website is for losers LOL!")
+// console.log(result);
+
+
+
+
+function getChar(c){
+    //   const result = c.charCodeAt(0)
+      return c.charCodeAt(0)
+    }
+    console.log(getChar(a))
