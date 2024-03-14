abcf1ec46d61cc30e346b0590a4bd08160b3b634
sahal2255 <sahalvv9656@gmail.com>
2024-03-14 09:45:48 +0530

    mar-14

--- mar-14.js
+++ mar-14.js
@@ -1,14 +1,26 @@
-function position(letter){
-  letter=letter.toLowerCase()
-  const alpha=letter.charCodeAt(0)-96
-  return `Position of alphabet: ${alpha}`
-}
-console.log(position('a'));
+// find the position alphabet
 
 
 
-function find(string){
-    alpheabet='abcdefghijklmnopqrstuvwxyz'
-    return `Position of alphabet:`+(alpheabet.indexOf(string)+1)
-}
-console.log(find('b'));
+
+
+// function position(letter){
+//   letter=letter.toLowerCase()
+//   const alpha=letter.charCodeAt(0)-96
+//   return `Position of alphabet: ${alpha}`
+// }
+// console.log(position('a'));
+
+
+
+// function find(string){
+//     alpheabet='abcdefghijklmnopqrstuvwxyz'
+//     return `Position of alphabet:`+(alpheabet.indexOf(string)+1)
+// }
+// console.log(find('b'));
+
+
+
+// find unique element in an array
+
+
