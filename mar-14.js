35959ce5bbe9a20e6793fc9f83b800a847f36987
sahal2255 <sahalvv9656@gmail.com>
2024-03-14 09:32:38 +0530

    mar-14

---
+++ mar-14.js
@@ -0,0 +1,14 @@
+function position(letter){
+  letter=letter.toLowerCase()
+  const alpha=letter.charCodeAt(0)-96
+  return `Position of alphabet: ${alpha}`
+}
+console.log(position('a'));
+
+
+
+function find(string){
+    alpheabet='abcdefghijklmnopqrstuvwxyz'
+    return `Position of alphabet:`+(alpheabet.indexOf(string)+1)
+}
+console.log(find('b'));
