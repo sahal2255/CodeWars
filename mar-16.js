90840e69fd7088e693a66b7fe2b294645e72c38c
sahal2255 <sahalvv9656@gmail.com>
2024-03-16 09:32:17 +0530

    mar-16

---
+++ mar-16.js
@@ -0,0 +1,9 @@
+var countSheep = function (num){
+    //your code here
+    let sheepCount=''
+    for(let i=1;i<=num;i++){
+       sheepCount +=i+' ' +'sheep...'
+    }
+    return sheepCount
+  }
+  console.log(countSheep(2));
